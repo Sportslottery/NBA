432e50151830cd9cfada771e32df4c206d39f501
ShaneKao <ko8342010@yahoo.com.tw>
2017-01-05 00:39:28 +0800

    init commit

--- crawler/espn_summary_crawler.py
+++ crawler/espn_summary_crawler.py
@@ -25,19 +25,21 @@ class EspnSummaryData(object):
         odds_details = doc('.odds-details').text()
         result_dict = {}
         if odds_details:
-            m1 = re.match(r'.*Line: (\S+) (\S+)', odds_details)
-            if m1:
-                result_dict['line_team'] = m1.group(1)
-                if m1.group(2) == 'EVEN':
-                    result_dict['line_margin'] = m1.group(2)
-                else:
+            if 'Line: EVEN' in odds_details:
+                result_dict['line_margin'] = 'EVEN'
+                if 'Over/Under:' in odds_details:
+                    result_dict['over_under'] = float(odds_details.split()[-1])
+            else:
+                m1 = re.match(r'.*Line: (\S+) (\S+)', odds_details)
+                if m1:
+                    result_dict['line_team'] = m1.group(1)
                     result_dict['line_margin'] = float(m1.group(2))
-            m2 = re.match(r'.*Over/Under: (\d+)', odds_details)
-            if m2:
-                result_dict['over_under'] = float(m2.group(1))
-        game_flow = doc('#gameFlow-graph').attr('data-plays')
-        if game_flow:
-            result_dict['game_flow'] = eval(game_flow)
+                m2 = re.match(r'.*Over/Under: (\d+)', odds_details)
+                if m2:
+                    result_dict['over_under'] = float(m2.group(1))
+            game_flow = doc('#gameFlow-graph').attr('data-plays')
+            if game_flow:
+                result_dict['game_flow'] = eval(game_flow)
         return result_dict
 
     def main(self, year):
