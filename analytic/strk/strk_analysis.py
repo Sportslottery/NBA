b8f1c038578d2d4f280727f7da1fe282cce7812f
ShaneKao <ko8342010@yahoo.com.tw>
2017-01-24 01:37:19 +0800

    init commit

--- analytic/strk/strk_analysis.py
+++ analytic/strk/strk_analysis.py
@@ -19,6 +19,9 @@ data['date'] = map(lambda x: time.mktime(datetime.strptime(x, '%Y-%m-%dT%H:%MZ')
     data['date'].tolist())
 data['two_way_winner'] = np.where(data['away_score'] > data['home_score'], data['away_abbreviation'], data['home_abbreviation'])
 data['home_total_strk'] = data['away_total_strk'] = data['home_home_strk'] = data['away_away_strk'] = [None for i in range(data.shape[0])]
+data['home_total_last_strk'] = data['away_total_last_strk'] = data['home_home_last_strk'] = data['away_away_last_strk'] = [None for i in range(data.shape[0])]
+
+
 
 def get_last_game_result(team_name, season_type, season_year, date, game_type ='total'):
     if game_type == 'total':
@@ -49,18 +52,60 @@ data['away_last_away_result'] = data.apply(lambda x: get_last_game_result(x['awa
                                                                           game_type = 'away') , axis=1)
 
 
-# def get_last_game_strk(team_name, season_type, season_year, date, game_type ='total')
+def get_last_game_strk(team_name, season_type, season_year, date, game_type ='total'):
+    if game_type == 'total':
+        cond = (data['away_abbreviation'] == team_name) | (data['home_abbreviation'] == team_name)
+    elif game_type == 'home':
+        cond = data['home_abbreviation'] == team_name
+    else:
+        cond = data['away_abbreviation'] == team_name
+    df = data[cond][(data['season_type'] == season_type) & (data['season_year'] == season_year)][data['date'] < date].tail(1)
+    if df.shape[0] == 0:
+        strk_result = None
+    else:
+        if df.iloc[0]['away_abbreviation'] == team_name:
+            strk_result = df.iloc[0]['away_total_strk']
+        else:
+            strk_result = df.iloc[0]['home_total_strk']
+    return strk_result
 
-for i in range(100): #data.shape[0]
+for i in range(data.shape[0]): #data.shape[0]
     print i
-    if not data.iloc[i]['home_last_total_result']:
-        data.set_value(i, 'home_total_strk', 0)
-    if not data.iloc[i]['away_last_total_result']:
-        data.set_value(i, 'away_total_strk', 0)
-    if not data.iloc[i]['home_last_home_result']:
-        data.set_value(i, 'home_home_strk', 0)
-    if not data.iloc[i]['away_last_away_result']:
-        data.set_value(i, 'away_away_strk', 0)
+    home_last_strk = get_last_game_strk(data.iloc[i]['home_abbreviation'],
+                                         data.iloc[i]['season_type'],
+                                         data.iloc[i]['season_year'],
+                                         data.iloc[i]['date'], game_type ='total')
+    away_last_strk = get_last_game_strk(data.iloc[i]['away_abbreviation'],
+                                         data.iloc[i]['season_type'],
+                                         data.iloc[i]['season_year'],
+                                         data.iloc[i]['date'], game_type ='total')
+    if home_last_strk == None:
+         data.set_value(i, 'home_total_strk', 0)
+    else:
+        if home_last_strk >= 0:
+            if data.iloc[i]['home_last_total_result'] == 'L':
+                data.set_value(i, 'home_total_strk', -1)
+            else:
+                data.set_value(i, 'home_total_strk', home_last_strk + 1)
+        else:
+            if data.iloc[i]['home_last_total_result'] == 'L':
+                data.set_value(i, 'home_total_strk', home_last_strk - 1)
+            else:
+                data.set_value(i, 'home_total_strk', 1)
+
+    if away_last_strk == None:
+         data.set_value(i, 'away_total_strk', 0)
+    else:
+        if away_last_strk >= 0:
+            if data.iloc[i]['away_last_total_result'] == 'L':
+                data.set_value(i, 'away_total_strk', -1)
+            else:
+                data.set_value(i, 'away_total_strk', away_last_strk + 1)
+        else:
+            if data.iloc[i]['away_last_total_result'] == 'L':
+                data.set_value(i, 'away_total_strk', away_last_strk - 1)
+            else:
+                data.set_value(i, 'away_total_strk', 1)
 
 
 data.to_csv('a.csv')
