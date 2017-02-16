e31d1cbc36ae6fdc48b886060f8dc511181f3f3c
ShaneKao <ko8342010@yahoo.com.tw>
2017-02-16 22:31:38 +0800

    init commit

--- analytic/back2back/back2back.py
+++ analytic/back2back/back2back.py
@@ -3,7 +3,7 @@ __author__ = 'shane'
 import os
 import time
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 import pandas as pd
@@ -21,3 +21,16 @@ for year in range(2006, 2017):
     data = data.append(api_df)
 
 data['date'] = data['date'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
+
+def back2back(team_name, date):
+    df = data[(data['away_abbreviation'] == team_name)|(data['home_abbreviation'] == team_name)]
+    df = df[df['date'] == date - timedelta(days=1)]
+    return df.shape[0]
+
+home_b2b = away_b2b = []
+for i in range(data.shape[0]):
+    home_b2b.append(back2back(data.iloc[i]['home_abbreviation'], data.iloc[i]['date']))
+    away_b2b.append(back2back(data.iloc[i]['away_abbreviation'], data.iloc[i]['date']))
+
+data['home_b2b'] = home_b2b
+data['away_b2b'] = away_b2b
