a0965ac377a92f73a89d10865396795213e81c87
Shane_Kao <Shane_Kao@asus.com>
2017-02-20 22:26:22 +0800

    init commit

--- analytic/fatigue/fatigue.py
+++ analytic/fatigue/fatigue.py
@@ -1,36 +1,40 @@
-__author__ = 'shane'
+__author__ = 'Shane_Kao'
 
 import os
 from datetime import datetime, timedelta
 
 import pandas as pd
 
-proj_dict = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-# proj_dict = 'F:/NBA'
+# proj_dict = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+proj_dict = 'F:/NBA'
 data = pd.DataFrame()
 for year in range(2006, 2017):
     api_df = pd.read_csv('%s/crawler/data/espn_api/%d.csv' %(proj_dict, year, ),
-                     usecols=['id', 'away_abbreviation', 'home_abbreviation', 'season_year',
-                              'away_score', 'home_score', 'season_type', 'date'])
+                     usecols=['id', 'away_abbreviation', 'home_abbreviation', 'date'])
     print year
     data = data.append(api_df)
 
 data['date'] = data['date'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
 
-def back2back(team_name, date):
+def get_fatigue(team_name, date):
     df = data[(data['away_abbreviation'] == team_name)|(data['home_abbreviation'] == team_name)]
-    df = df[df['date'] == date - timedelta(days=1)]
-    return df.shape[0]
+    df = df[df['date'] < date]
+    df = df[df['date'] >= (date - timedelta(days=6))]
+    N = df.shape[0]
+    return N
+#     return df.shape[0]
 
-home_b2b = []
-away_b2b = []
+# get_fatigue('OKC', datetime.strptime('2017-01-01', '%Y-%m-%d'))
+
+home_fatigue = []
+away_fatigue = []
 for i in range(data.shape[0]):
     print i, data.shape[0]
-    home_b2b.append(back2back(data.iloc[i]['home_abbreviation'], data.iloc[i]['date']))
-    away_b2b.append(back2back(data.iloc[i]['away_abbreviation'], data.iloc[i]['date']))
+    home_fatigue.append(get_fatigue(data.iloc[i]['home_abbreviation'], data.iloc[i]['date']))
+    away_fatigue.append(get_fatigue(data.iloc[i]['away_abbreviation'], data.iloc[i]['date']))
 
-data['home_b2b'] = home_b2b
-data['away_b2b'] = away_b2b
+data['home_fatigue'] = home_fatigue
+data['away_fatigue'] = away_fatigue
 
 for year in range(2007, 2017):
     df = pd.read_csv('%s/analytic/standing/data/%d.csv' %(proj_dict, year, ))
@@ -41,8 +45,8 @@ for year in range(2007, 2017):
         df = df[['id', 'season_type', 'two_way_winner', 'over_under_result',
              'home_line_margin', 'over_under']]
 
-    df = pd.merge(df, data.loc[:, ['id', u'home_b2b', u'away_b2b']], on='id', how='left')
+    df = pd.merge(df, data.loc[:, ['id', u'home_fatigue', u'away_fatigue']], on='id', how='left')
 
 
-    df.to_csv(proj_dict + '/analytic/back2back/data/' + '%d.csv' %year, index=None)
+    df.to_csv(proj_dict + '/analytic/fatigue/data/' + '%d.csv' %year, index=None)
     print year
